ebb4c800e3374e6aea1f49360d4725d766ca9cd8
Kristi B <60147968+kristilb1999@users.noreply.github.com>
2021-11-18 15:24:06 -0500

    Updated model code to match database

---
+++ Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/model/User.java
@@ -0,0 +1,47 @@
+package com.mthree.cryptoinvesting.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getId() == user.getId() && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getPassword(), user.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getPassword());
+    }
+}
