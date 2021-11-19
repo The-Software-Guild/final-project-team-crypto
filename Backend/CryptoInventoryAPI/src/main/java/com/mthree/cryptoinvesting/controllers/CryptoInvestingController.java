35b463e4031c271c0fabeda5efe0fe1a62987d5e
Kristi B <60147968+kristilb1999@users.noreply.github.com>
2021-11-19 13:29:25 -0500

    Started mapping out controller and service layer interface

--- Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/controllers/CryptoInvestingController.java
+++ Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/controllers/CryptoInvestingController.java
@@ -1,5 +1,34 @@
 package com.mthree.cryptoinvesting.controllers;
 
+import com.mthree.cryptoinvesting.service.CryptoInvestingServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author Kristi
+ * @version 11/19/2021
+ */
+
+
+@RestController
+@RequestMapping("/api/cryptoinventory")
 public class CryptoInvestingController {
+
+    @Autowired
+    CryptoInvestingServiceLayer service;
+
+    @PostMapping("/begin")
+    @ResponseStatus(HttpStatus.CREATED)
+    public int createUser(){
+        return service.newUser();
+    }
+
+    @GetMapping("/cryptos/{portfolioId}")
+    public List<String> getAllCryptos(@PathVariable("portfolioId") int portfolioId) {
+        return service.getAllCryptos(portfolioId);
+    }
 
 }
--- Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/service/CryptoInvestingServiceLayer.java
+++ Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/service/CryptoInvestingServiceLayer.java
@@ -1,7 +1,12 @@
 package com.mthree.cryptoinvesting.service;
 
+
+import java.util.List;
+
 public interface CryptoInvestingServiceLayer {
 
+    int newUser();
 
+    List<String> getAllCryptos(int portfolioId);
 
 }
