9da3c26da7c2fbb60f4594a5ca7d0e626850011c
Miguel619 <mmellado@mail.sfsu.edu>
2021-11-19 12:29:07 -0800

    redirected / to home

--- Frontend/crypto-app/src/app/home/home-routing.module.ts
+++ Frontend/crypto-app/src/app/home/home-routing.module.ts
@@ -5,7 +5,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomePageComponent}
+  {path: 'home', component: HomePageComponent},
+  { path: '' , redirectTo:'/home', pathMatch:'full' }
 ];
 
 @NgModule({
