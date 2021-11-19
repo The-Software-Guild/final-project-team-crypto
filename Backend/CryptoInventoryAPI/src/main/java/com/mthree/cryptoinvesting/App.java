224eb67ce66a830d49a5c312ccf107d8241386c5
Kristi B <60147968+kristilb1999@users.noreply.github.com>
2021-11-19 13:31:14 -0500

    Added app.java code for springboot

--- Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/App.java
+++ Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/App.java
@@ -1,5 +1,16 @@
 package com.mthree.cryptoinvesting;
 
-public class App {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Kristi
+ * @version 11/19/2021
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 }
--- Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/service/CryptoInvestingServiceLayer.java
+++ Backend/CryptoInventoryAPI/src/main/java/com/mthree/cryptoinvesting/service/CryptoInvestingServiceLayer.java
@@ -3,6 +3,11 @@ package com.mthree.cryptoinvesting.service;
 
 import java.util.List;
 
+/**
+ * @author Kristi
+ * @version 11/19/2021
+ */
+
 public interface CryptoInvestingServiceLayer {
 
     int newUser();
