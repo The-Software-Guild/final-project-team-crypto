7a5a710f901dcd07e735cb3296e019a28b83ccf4
Miguel619 <mmellado@mail.sfsu.edu>
2021-11-22 13:56:42 -0800

    removed search bar

--- Frontend/crypto-app/src/app/app.module.ts
+++ Frontend/crypto-app/src/app/app.module.ts
@@ -8,13 +8,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeModule } from './home/home.module';
 import { AccountModule } from './account/account.module';
 import { AboutusModule } from './aboutus/aboutus.module';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { CryptolistRoutingModule } from './cryptolist/cryptolist-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchBarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
