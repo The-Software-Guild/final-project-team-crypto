fd1177683c3232fcf658dfb8c05e5f5308d9798e
Miguel619 <miguelmellado619@gmail.com>
2021-11-22 09:45:51 -0800

    Revert "Revert "rough account page draft""
    This reverts commit 4c6607b16657434e70654e5e5dc91e859115cb56.

--- Frontend/crypto-app/src/app/account/account.module.ts
+++ Frontend/crypto-app/src/app/account/account.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
 import { AccountPageComponent } from './account-page/account-page.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 
 @NgModule({
   declarations: [
-    AccountPageComponent
+    AccountPageComponent,
+    AccountDetailsComponent,
+    OrderDetailsComponent
   ],
   imports: [
     CommonModule,
