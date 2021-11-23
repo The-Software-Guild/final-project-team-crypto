ed8b908e1f6a205b0e6170acc9895e55d3838d90
Kristi B <60147968+kristilb1999@users.noreply.github.com>
2021-11-23 14:02:46 -0500

    created files for testing profile and user dao

---
+++ Backend/CryptoInventoryAPI/src/test/java/com/mthree/cryptoinvesting/data/UserDaoDatabaseImplTest.java
@@ -0,0 +1,11 @@
+package com.mthree.cryptoinvesting.data;
+
+import com.mthree.cryptoinvesting.TestApplicationConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestApplicationConfiguration.class)
+public class UserDaoDatabaseImplTest {
+}
