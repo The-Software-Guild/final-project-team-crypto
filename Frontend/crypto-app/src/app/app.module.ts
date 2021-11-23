07c5a60469f216f8ac3f2092f54a9de59e5248bd
Miguel619 <mmellado@mail.sfsu.edu>
2021-11-22 16:04:39 -0800

    Added login and sign up

--- Frontend/crypto-app/src/app/app.module.ts
+++ Frontend/crypto-app/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { HomeModule } from './home/home.module';
 import { AccountModule } from './account/account.module';
 import { AboutusModule } from './aboutus/aboutus.module';
 import { CryptolistRoutingModule } from './cryptolist/cryptolist-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { LoginModule} from './login/login.module';
+import {SignupModule} from './signup/signup.module';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +28,17 @@ import { CryptolistRoutingModule } from './cryptolist/cryptolist-routing.module'
     HomeModule,
     AccountModule,
     AboutusModule,
-    CryptolistRoutingModule
+    CryptolistRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    LoginModule,
+    SignupModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/crypto-app/src/app/login/login-page/login-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
+import { AuthenticationService } from '../../auth/authentication.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router
+    ) { }
+
+    ngOnInit(): void {
+      this.loginForm = new FormGroup({
+        email: new FormControl(null, [Validators.required, Validators.email, Validators.minLength(6)]),
+        password: new FormControl(null, [Validators.required, Validators.minLength(3)])
+      })
+    }
+
+    onSubmit() {
+      if(this.loginForm.invalid) {
+        return;
+      }
+      console.log(this.loginForm.value);
+      // this.authService.login(this.loginForm.value).pipe(
+      //   map(token => this.router.navigate(['account']))
+      // ).subscribe()
+      
+    }
+
+}
