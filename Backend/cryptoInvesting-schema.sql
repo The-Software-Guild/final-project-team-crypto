d3d73a7af6d7215b538cc872c7f2423f9ccf5885
Miguel619 <mmellado@mail.sfsu.edu>
2021-11-23 11:42:23 -0800

    Updated second schema

--- Backend/cryptoInvesting-schema.sql
+++ Backend/cryptoInvesting-schema.sql
@@ -13,11 +13,11 @@ CREATE TABLE user (
 CREATE TABLE portfolio (
     portfolioId INT PRIMARY KEY AUTO_INCREMENT,
     userId INT,
-    BTC float NOT NULL DEFAULT 0,
-    ETH float NOT NULL DEFAULT 0,
-    BNB float NOT NULL DEFAULT 0,
-    ADA float NOT NULL DEFAULT 0,
-    DOGE float NOT NULL DEFAULT 0,
+    BTC FLOAT NOT NULL DEFAULT 0,
+    ETH FLOAT NOT NULL DEFAULT 0,
+    BNB FLOAT NOT NULL DEFAULT 0,
+    ADA FLOAT NOT NULL DEFAULT 0,
+    DOGE FLOAT NOT NULL DEFAULT 0,
     CONSTRAINT fk_user_portfolio 
     	FOREIGN KEY (userId)
     	REFERENCES user(userId)
@@ -37,3 +37,19 @@ CREATE TABLE orders (
     	REFERENCES portfolio(portfolioId)
 		ON DELETE CASCADE
 );
+
+CREATE TABLE market (
+    ticker VARCHAR(5) NOT NULL,
+    price FLOAT NOT NULL DEFAULT 0
+    
+);
+insert into market (ticker, price) values ("BTC", 0);
+insert into market (ticker, price) values ("ETH", 0);
+insert into market (ticker, price) values ("BNB", 0);
+insert into market (ticker, price) values ("ADA", 0);
+insert into market (ticker, price) values ("DOGE", 0);
+
+insert into user (username, password) values ("Miguel", "1234");
+insert into user (username, password) values ("Chelsey", "9632");
+insert into user (username, password) values ("Kristi", "54321");
+insert into user (username, password) values ("Will", "54624");
