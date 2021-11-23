ee9d0ad1c57d8955829cd780a3cd4686354bd517
Chelsey Bergmann <chelseybergmann1@gmail.com>
2021-11-23 10:43:45 -0600

    OrderDAO methods testing - all work

--- Backend/CryptoInventoryAPI/src/test/java/com/mthree/cryptoinvesting/data/OrderDaoDatabaseImplTest.java
+++ Backend/CryptoInventoryAPI/src/test/java/com/mthree/cryptoinvesting/data/OrderDaoDatabaseImplTest.java
@@ -1,10 +1,13 @@
+/**
+ * @author Chelsey
+ * Tests the OrderDaoDatabaseImpl file.  You must run each test method
+ * one by one in the order they are presented.
+ */
 package com.mthree.cryptoinvesting.data;
-
 import com.mthree.cryptoinvesting.TestApplicationConfiguration;
 import com.mthree.cryptoinvesting.model.Orders;
 import com.mthree.cryptoinvesting.model.Portfolio;
 import com.mthree.cryptoinvesting.model.Users;
-import org.apache.catalina.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +17,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -89,4 +93,66 @@ public class OrderDaoDatabaseImplTest {
             assertEquals(order.getDateSold(),null);
     }
 
+    @Test
+    public void testGetAllOrdersByPortfolioId() {
+        //add order one
+        Orders order2 = new Orders();
+        order2.setPortfolioId(1);
+        order2.setCryptoName("ETH");
+        order2.setPrice(50f);
+        order2.setAmount(5);
+        order2.setDatePurchased(LocalDateTime.now());
+        orderTest.addOrder(order2);
+
+        List<Orders> userOrders = orderTest.getAllOrdersByPortfolioId(1);
+
+        for (Orders order : userOrders) {
+            assertEquals(order.getPortfolioId(),1);
+        }
+    }
+
+    @Test
+    public void getAllOrders() {
+        List<Orders> allOrders = orderTest.getAllOrders();
+
+        assertEquals(allOrders.size(),2);
+
+        Users user2 = new Users();
+        user2.setUsername("hello.world");
+        user2.setPassword("1soifsi");
+        userTest.addUser(user2);
+
+        //add portfolio
+        Portfolio portfolio2 = portfolioTest.addPortfolio(user2);
+
+        // Add a new order for a new user, check size and portfolio id.
+        Orders order3 = new Orders();
+        order3.setPortfolioId(user2.getUserId());
+        order3.setCryptoName("ADA");
+        order3.setPrice(100f);
+        order3.setAmount(2);
+        order3.setDatePurchased(LocalDateTime.now());
+        orderTest.addOrder(order3);
+
+        allOrders = orderTest.getAllOrders();
+
+        assertEquals(allOrders.size(),3);
+    }
+
+    @Test
+    public void testRemoveOrder() {
+        Orders order = orderTest.getOrderById(1);
+        Orders removed = orderTest.removeOrder(order);
+
+        List<Orders> allOrders = orderTest.getAllOrders();
+
+        for (Orders o : allOrders) {
+            if (o.getOrderId() == 1) {
+                assertEquals(o.getDateSold() != null, true);
+            } else {
+                assertEquals(o.getDateSold() == null, true);
+            }
+        }
+
+    }
 }
